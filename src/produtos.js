a93f0a7234083e16f8c62bb637ddc046330a1616
Cristiane de Morais <chryk@hotmail.com>
2021-11-11 19:33:40 +1300

    inclusao pagina index com jason
    criacao de lista de produtos usando json

---
+++ src/produtos.js
@@ -0,0 +1,49 @@
+var produtos = {
+  dados: [
+    {   
+      id: 1,
+        titulo: 'Blusa Trees Masculina',
+        descricao: '',
+        imagem: 'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta.jpg',
+        categoria: 'Blusas',
+        preco: 'R$ 89,00'
+
+    }, {
+      id: 2,
+        titulo: 'Bolsa de Lona Feminina',
+        descricao: '',
+        imagem: 'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%202.jpg',
+        categoria: 'Bolsas',
+        preco: 'R$ 129,90'
+
+    }, {
+      id: 3,
+        titulo: 'Blusa de Algodão Orgânico',
+        imagem: 'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%203.jpg',
+        descricao: '',
+        categoria: 'Bolsas',
+        preco: 'R$115,90'
+    }, {
+      id: 4,
+        titulo: 'Short Vintage Feminina',
+        imagem:'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%205.jpg',
+        descricao: '',
+        categoria: 'Shorts',
+        preco: 'R$120,00'
+    }, {
+      id: 5,
+        titulo: 'Bolsa Pinturas de Obras',
+        imagem:'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%204.jpg',
+        descricao: '',
+        categoria: 'Bolsas',
+        preco: 'R$55,40'
+    }, {
+      id: 6,
+        titulo: 'Moleton Harry Potter',
+        imagem:'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%206.jpg',
+        descricao: '',
+        categoria: 'Moleton',
+        preco: 'R$159,90'
+    }
+  ],
+}
