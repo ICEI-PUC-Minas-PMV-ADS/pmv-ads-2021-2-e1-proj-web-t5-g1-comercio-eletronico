015a669b6a46c8bc249c79677c974812b78f46e1
Cristiane de Morais <chryk@hotmail.com>
2021-11-15 20:48:38 +1300

    update
    adicionar produto com javascript

--- src/produtos.js
+++ src/produtos.js
@@ -1,49 +1,55 @@
 var produtos = {
   dados: [
     {   
-      id: 1,
+      id: 0,
         titulo: 'Blusa Trees Masculina',
         descricao: '',
         imagem: 'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta.jpg',
         categoria: 'Blusas',
-        preco: 'R$ 89,00'
+        preco: 'R$ 89,00',
+        quantidade: 0,
 
     }, {
-      id: 2,
+      id: 1,
         titulo: 'Bolsa de Lona Feminina',
         descricao: '',
         imagem: 'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%202.jpg',
         categoria: 'Bolsas',
-        preco: 'R$ 129,90'
+        preco: 'R$ 129,90',
+        quantidade: 0,
 
     }, {
-      id: 3,
+      id: 2,
         titulo: 'Blusa de Algodão Orgânico',
         imagem: 'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%203.jpg',
         descricao: '',
         categoria: 'Bolsas',
-        preco: 'R$115,90'
+        preco: 'R$115,90',
+        quantidade: 0,
     }, {
-      id: 4,
+      id: 3,
         titulo: 'Short Vintage Feminina',
         imagem:'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%205.jpg',
         descricao: '',
         categoria: 'Shorts',
-        preco: 'R$120,00'
+        preco: 'R$120,00',
+        quantidade: 0,
     }, {
-      id: 5,
+      id: 4,
         titulo: 'Bolsa Pinturas de Obras',
         imagem:'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%204.jpg',
         descricao: '',
         categoria: 'Bolsas',
-        preco: 'R$55,40'
+        preco: 'R$55,40',
+        quantidade: 0,
     }, {
-      id: 6,
+      id: 5,
         titulo: 'Moleton Harry Potter',
         imagem:'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2021-2-e1-proj-web-t5-g1-comercio-eletronico/main/src/Oferta%206.jpg',
         descricao: '',
         categoria: 'Moleton',
-        preco: 'R$159,90'
+        preco: 'R$159,90',
+        quantidade: 0,
     }
   ],
 }
