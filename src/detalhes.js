b335bfb3d0d77db3f420498afc39f4e29153bcc9
Cristiane de Morais <chryk@hotmail.com>
2021-11-28 10:50:32 +1300

    Update detalhes.js
    update

--- src/detalhes.js
+++ src/detalhes.js
@@ -13,33 +13,7 @@ for (let i=0; i < detalheProduto.length; i++) {
 }
 
 
-function exibirDetalhes() {
-    
 
-    let detalheProduto = localStorage.getItem('detalheProduto')
-    detalheProduto = JSON.parse(mostraConteudo);
-    let mostraConteudo = localStorage.getItem('detalheProduto');
-
-    if (detalhamentoProduto == null) {
-    mostraConteudo = '';
-        Object.values(detalheProduto).map(produtos => {
-                mostraConteudo.innerHTML += `
-                <div class="imagem-produto">
-                    <img src="${produtos.imagem}" alt="${produtos.titulo}" width="160" height="160" class="foto-produto-detalhe"> <br> 
-                    <span> ${produtos.titulo} </span>
-                </div>
-                <div class="preco-produto">  R$${produtos.preco},00 </div>
-                <div class="detalhes-produto"> 
-                    <span>${produtos.descricao}</span>
-                </div>
-                `;
-        });
-    }
-  
-    document.querySelector('.detalhes-produto').innerHTML = mostraConteudo
-}
-
-       
    
 
 
